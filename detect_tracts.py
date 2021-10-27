cd01853c2ef535b5240bebb8818c4bec8c5b797e
Martin Petr <git@bodkan.net>
2021-10-27 10:23:22 +0200

    Implement basic ancestry tracts detection

--- detect_tracts.py
+++ detect_tracts.py
@@ -1,13 +1,36 @@
-import subprocess, msprime, pyslim
-import matplotlib.pyplot as plt
-import numpy
+import argparse
+import pathlib
+import sys
+import os
+
+import tskit
+import pyslim
 import pandas
+import numpy
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--trees", required=True, metavar="FILE", help="Tree sequence file")
+parser.add_argument("--model", required=True, metavar="DIRECTORY", help="slendr model directory")
+parser.add_argument("--source-label", required=True, type=str,
+                    help="Introgressing population label")
+parser.add_argument("--output", required=True, metavar="FILE",
+                    help="Output file with coordinates of introgressed tracts")
+args = parser.parse_args("--trees results/output_ts.trees --model model/ --source-label NEA --output out.txt".split())
+
+model_dir = os.path.expanduser(args.model)
+
+if not os.path.exists(model_dir):
+    sys.exit(f"Model directory {model_dir} does not exist")
+
+populations_path = pathlib.Path(model_dir, "populations.tsv")
+source_id = pandas.read_table(populations_path).query(f"pop == '{args.source_label}'").pop_id[0]
 
 ts = pyslim.load("test.trees")
 
 # Load the .trees file and assess true local ancestry
-breaks = np.zeros(ts.num_trees + 1)
-ancestry = np.zeros(ts.num_trees + 1)
+breaks = numpy.zeros(ts.num_trees + 1)
+ancestry = numpy.zeros(ts.num_trees + 1)
 
 tree = ts.first()
 # iterate over trees and ...
